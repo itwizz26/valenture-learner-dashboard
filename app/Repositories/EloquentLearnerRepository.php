b7c8382856a97f1e29c0ebaa76d98d9c642d72d5
Daniel - Itwizz26 <itwizz26@gmail.com>
2026-02-01 13:18:11 +0200

    feat: implement sorting in Repository

--- app/Repositories/EloquentLearnerRepository.php
+++ app/Repositories/EloquentLearnerRepository.php
@@ -5,13 +5,24 @@ namespace App\Repositories;
 use App\Models\Learner;
 
 class EloquentLearnerRepository implements LearnerRepositoryInterface {
-    public function getLearnersWithProgress(array $filters = [], string $sortBy = null) {
-        $query = Learner::with(['courses']);
+
+    public function getLearnersWithProgress(array $filters = [], string $sortBy = null) 
+    {
+        $query = Learner::query()
+            ->select('learners.*')
+            ->with(['courses']);
 
         if (!empty($filters['course_id'])) {
             $query->whereHas('courses', function ($q) use ($filters) {
                 $q->where('courses.id', $filters['course_id']);
             });
+        }
+
+        if ($sortBy === 'progress_asc' || $sortBy === 'progress_desc') {
+            $direction = $sortBy === 'progress_desc' ? 'DESC' : 'ASC';
+            
+            $query->leftJoin('enrolments', 'learners.id', '=', 'enrolments.learner_id')
+                ->orderBy('enrolments.progress', $direction);
         }
 
         return $query->get();
